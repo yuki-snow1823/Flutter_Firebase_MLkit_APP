eafdfae84562cdd8cb68b21c321f59e388939c0f
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2021-08-15 01:30:31 +0900

    スライダー

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-/* 3-3. スイッチ */
+/* 3-4. スライダー */
 
 import 'package:flutter/material.dart';
 
@@ -27,11 +27,11 @@ class MyForm extends StatefulWidget {
 }
 
 class _MyFormState extends State<MyForm> {
-  bool _active = false;
+  double _value = 0.0;
 
-  void _changeSwitch(bool e) {
+  void _changeSlider(double e) {
     setState(() {  // 状態を保持する変数を変更する処理は、setState内に記述する
-      _active = e;
+      _value = e;
     });
   }
 
@@ -40,16 +40,16 @@ class _MyFormState extends State<MyForm> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Center(
-              child: Icon(
-                Icons.thumb_up,
-                color: _active ? Colors.orange : Colors.grey,
-                size: 100.0,
-              ),
-            ),
-            Switch(  // スイッチ
-              value: _active,
-              onChanged: _changeSwitch,
+            Center(child:Text("値：${_value}")),
+            Slider(  // スライダー
+              label: '${_value}',
+              min: 0,
+              max: 10,
+              value: _value,
+              activeColor: Colors.orange,
+              inactiveColor: Colors.blueAccent,
+              divisions: 10,
+              onChanged: _changeSlider,
             )
           ],
         )
