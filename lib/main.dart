9d1093351c49bc884baad6f201d6b9de6abaab7a
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2021-08-15 01:02:08 +0900

    レイアウトの学習

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,25 @@
-/* 1-4. ListView */
+/* 2-1. Container */
 
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp()); // 引数のWidgetが画面いっぱいに表示される
 
-// 状態を管理するクラスは、Stateクラスを継承
-class RandomWordsState extends State<RandomWords> {
-  // _を付けるとライブラリ外からアクセスできなくなる
-  // ListViewのWidgetを作るメソッド
-  Widget _buildSuggestions() {
-    final _wordPairs = <WordPair>[];  // 単語のペアを格納するリスト
-    return ListView.builder(
-      itemBuilder: (context, i) { // itemBuilderで一行ごとに処理が呼ばれる
-        if (i.isOdd) return Divider();  // 奇数行には水平線を表示
-        print("読み込まれました");
-        final index = i ~/ 2;  // ~/は結果が整数の割り算
-        if (index >= _wordPairs.length) {  // 行数がリストの要素数を越えれば
-          _wordPairs.addAll(generateWordPairs().take(10));  // 単語のペアを10個追加
-        }
-        return _buildRow(_wordPairs[index]);
-      }
-    );
-  }
-
-  // 単語のペアから、形式を整えた行のWidgetを作るメソッド
-  Widget _buildRow(WordPair pair) {
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-      ),
-    );
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Live!人工知能"),
+    return MaterialApp(
+      title: "My Simple App",
+      home: Scaffold(
+        body: Center(
+          child: Container(
+            color: Colors.blue,
+            width: 300.0,
+            height: 300.0,
+            child: Text("Live!人工知能"),
+            alignment: Alignment.center,
+          ),
+        ),
       ),
-      body: _buildSuggestions(),
-    );
-  }
-}
-
-// StatefulなWidgetのクラスは、StatefulWidgetを継承
-class RandomWords extends StatefulWidget {
-  @override
-  RandomWordsState createState() => RandomWordsState();
-}
-
-// 最初に表示するWidgetのクラス
-class MyApp extends StatelessWidget {  // StatelessWidgetを継承
-  @override
-  Widget build(BuildContext context) {  //buildメソッドでUIを作成
-    return MaterialApp(  // マテリアルデザインのアプリ
-      title: "My Simple App",  // アプリのタイトル
-      home: RandomWords()
     );
   }
 }
