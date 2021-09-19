b94f0e6400a7d4c38884baa5d6365adbd8f37372
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2021-09-19 10:19:14 +0900

    add: 顔画像認証

--- lib/main.dart
+++ lib/main.dart
@@ -1,75 +1,86 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-void main()  async{
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Dog Name Voting",
-      home: MyHomePage(),
+      title: "Face Finder",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: FaceFinder(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class FaceFinder extends StatefulWidget {
   @override
-  _MyHomePageState createState() {
-    return _MyHomePageState();
-  }
+  State<StatefulWidget> createState() => _FaceFinderState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _FaceFinderState extends State<FaceFinder> {
+  File _imageFile;
+  final ImagePicker _picker = ImagePicker();
+
+  void _getImageAndFindFace(ImageSource imageSource) async {
+    setState(() {
+      _imageFile = null;
+    });
+
+    final PickedFile pickedImage = await _picker.getImage(source: imageSource);
+    final File imageFile = File(pickedImage.path);
+
+    setState(() {
+      _imageFile = imageFile;
+    });
+  }
+
+  Widget _makeImage() {
+    return Container(
+      constraints: BoxConstraints.expand(),
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: Image.file(_imageFile).image,
+          fit: BoxFit.contain,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Dog Name Voting")),
-      body: _buildBody(),
-    );
-  }
-
-  Widget _buildBody() {
-    return StreamBuilder<QuerySnapshot>(  // Streamを監視して、イベントが通知される度にWidgetを更新する
-      stream: FirebaseFirestore.instance.collection("dogs").snapshots(),
-      builder: (context, snapshot) {
-        if (!snapshot.hasData) return LinearProgressIndicator();
-        return _buildList(snapshot.data.docs);
-      },
-    );
-  }
-
-  Widget _buildList(List<DocumentSnapshot> snapList) {
-    return ListView.builder(
-        padding: const EdgeInsets.all(18.0),
-        itemCount: snapList.length,
-        itemBuilder: (context, i) {
-          return _buildListItem(snapList[i]);
-        }
-    );
-  }
-
-  Widget _buildListItem(DocumentSnapshot snap) {
-    Map<String, dynamic> data = snap.data();
-
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 18.0, vertical:9.0),
-      child: Container(
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.grey),
-          borderRadius: BorderRadius.circular(8.0),
-        ),
-        child: ListTile(
-          title: Text(data["name"]),
-          trailing: Text(data["votes"].toString()),
-          onTap: () => snap.reference.update({"votes": FieldValue.increment(1)}),
-        ),
+      appBar: AppBar(
+        title: Text("Face Finder"),
       ),
+      body: _imageFile == null
+          ? Center(child: Text("No image selected."))
+          : _makeImage(),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          FloatingActionButton(
+            onPressed:(){_getImageAndFindFace(ImageSource.gallery);} ,
+            tooltip: "Select Image",
+            child: Icon(Icons.add_photo_alternate),
+          ),
+          Padding(padding: EdgeInsets.all(10.0)),
+          FloatingActionButton(
+            onPressed:(){_getImageAndFindFace(ImageSource.camera);} ,
+            tooltip: "Take Photo",
+            child: Icon(Icons.add_a_photo),
+          ),
+        ],
+      )
     );
   }
 }
